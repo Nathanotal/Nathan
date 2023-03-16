4c42cf9fbde459240bea53a82992756fcad22b1c
Nathanotal <nathan.allard@gmail.com>
2023-03-16 20:44:45 +0100

    test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import anime from 'animejs/lib/anime.es.js';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		anime({
+			targets: 'div',
+			translateX: 250,
+			backgroundColor: '#FFF',
+			duration: 800
+		});
+	});
+</script>
+
+<html lang="english">
+	<head>
+		<title>Nathan</title>
+	</head>
+	<body>
+		<div>
+			<h1>Nathan</h1>
+		</div>
+	</body>
+</html>
+
+<style>
+	h1 {
+		color: red;
+	}
+</style>
---
+++ todo.md
@@ -0,0 +1,3 @@
+# Todo
+
+- [ ] Try [this](https://www.youtube.com/watch?v=ePFw62HISRI)
