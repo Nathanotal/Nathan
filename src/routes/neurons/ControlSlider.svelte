8094075b9612900557feab131b83248ffbfea346
Nathanotal <nathan.allard@gmail.com>
2023-04-27 12:03:09 +0200

    Add control slider

---
+++ src/routes/neurons/ControlSlider.svelte
@@ -0,0 +1,14 @@
+<script lang='ts'>
+    export let params: any;
+    export let paramControls: any;
+    export let paramType: any;
+    export let update_params: any;
+</script>
+
+<div class="flex flex-col">
+    <div class="flex justify-between">
+        <span>{paramControls[paramType].alias}</span>
+        <span>{params[paramType]}</span>
+    </div>
+    <input type="range" min={paramControls[paramType].min} max={paramControls[paramType].max} bind:value={params[paramType]} class="range" step={paramControls[paramType].step} on:input={(event) => update_params(event, paramType)}/>
+</div>
