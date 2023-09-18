93cebac1ec24ef9b9a1b1bbe5d66a6e5e7395516
Nathanotal <nathan.allard@gmail.com>
2023-09-18 09:55:00 +0200

    Debug

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,9 @@
 	import { inject } from '@vercel/analytics';
 	
 	inject({ mode: dev ? 'development' : 'production' });
+
+	console.log(dev);
+	console.log({ mode: dev ? 'development' : 'production' });
 </script>
 
 <Header />
